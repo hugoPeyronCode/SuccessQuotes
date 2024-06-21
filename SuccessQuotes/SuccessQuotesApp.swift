dbebb6a6fb1d9215bc5a8aa49175916c93392ab1
Hugo Peyron <hugopeyron@gmail.com>
2024-06-21 23:57:11 +0200

    Initial Commit

---
+++ SuccessQuotes/SuccessQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuccessQuotesApp.swift
+//  SuccessQuotes
+//
+//  Created by Hugo Peyron on 21/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct SuccessQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
